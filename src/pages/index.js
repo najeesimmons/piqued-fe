72b9ad6635a95b869344d3bb7995cc7dbc938461
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-09 21:44:51 -0500

    add components folder, update imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,28 @@
-import SearchBar from "@/SearchBar/SearchBar";
-import Section from "@/Section/Section";
+import SearchBar from "@/components/SearchBar/SearchBar";
+import Section from "@/components/Section/Section";
 import dynamic from "next/dynamic";
-// import PhotoMasonry from "@/Masonry/PhotoMasonry";
 import { createClient } from "pexels";
 require("dotenv").config();
 
-const DynamicPhotoMasonry = dynamic(() => import("@/Masonry/PhotoMasonry"), {
-  ssr: false,
-});
+const DynamicPhotoMasonry = dynamic(
+  () => import("@/components/Masonry/PhotoMasonry"),
+  {
+    ssr: false,
+  }
+);
 
 export async function getStaticProps() {
   const client = createClient(process.env.NEXT_PUBLIC_PEXELS_API_KEY);
   try {
     const response = await client.photos.curated({ per_page: 50 });
-    console.log("success:", response.photos[0]);
+    // console.log("success:", response.photos[0]);
     return {
       props: { photos: response.photos },
       revalidate: 3600, // Regenerates the page at most once per hour
     };
   } catch (error) {
     console.log(
-      "issue getting photos in getStaticProps, nobody would even see this"
+      "problem getting photos in getStaticProps, nobody would even see this"
     );
     return {
       props: { photos: [] }, // Fallback empty array
@@ -32,7 +34,6 @@ export async function getStaticProps() {
 export default function Home({ photos }) {
   return (
     <>
-      {/* <section className="w-full"></section> */}
       <Section>
         <SearchBar />
       </Section>
