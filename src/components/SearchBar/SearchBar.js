cd97f742fb0c4da936fa4a116d7975329eaa49b2
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-11 22:56:29 -0500

    search bar works

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,24 +1,17 @@
-import { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 
-function SearchBar() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const handleClick = () => {
-    console.log("i was clicked");
-    // doSearch(searchQuery);
-  };
-
+function SearchBar({ getSearchPhotos, searchTerm, setSearchTerm }) {
   return (
     <div className="flex h-8">
       <input
         className="mb-4 border-2 p-4 flex-grow"
         placeholder="Search..."
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <button
         className="w-1/12 flex items-center justify-center border-2 border-l-0"
-        onClick={handleClick}
+        onClick={getSearchPhotos}
       >
         <IoIosSearch size={25} />
       </button>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import SearchBar from "@/components/SearchBar/SearchBar";
 import Section from "@/components/Section/Section";
 import dynamic from "next/dynamic";
@@ -17,23 +18,48 @@ export async function getStaticProps() {
   try {
     const response = await client.photos.curated({ per_page: 80 });
     return {
-      props: { photos: response.photos },
+      props: { initPhotos: response.photos },
       revalidate: 3600,
     };
   } catch (error) {
     console.log("ERROR: getStaticProps... nobody can see this 👀");
     return {
-      props: { photos: [] },
+      props: { initPhotos: [] },
       revalidate: 3600,
     };
   }
 }
 
-export default function Home({ photos }) {
+export default function Home({ initPhotos }) {
+  const [photos, setPhotos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const getSearchPhotos = async () => {
+    console.log("in get search photos 👋🏾");
+    const client = createClient(process.env.NEXT_PUBLIC_PEXELS_API_KEY);
+    try {
+      const query = searchTerm;
+      const response = await client.photos.search({ query, per_page: 20 });
+      console.log("we've got photos 🚀:", response.photos[0]);
+      setPhotos(response.photos);
+    } catch (error) {
+      console.log(error.message);
+      console.log("no photos 👎🏾");
+    }
+  };
+
+  useEffect(() => {
+    if (!initPhotos) return;
+    setPhotos(initPhotos);
+  }, [initPhotos]);
+
   return (
     <>
       <Section>
-        <SearchBar />
+        <SearchBar
+          getSearchPhotos={getSearchPhotos}
+          setSearchTerm={setSearchTerm}
+          searchTerm={searchTerm}
+        />
       </Section>
       <Section>
         <DynamicPhotoMasonry photos={photos} />
