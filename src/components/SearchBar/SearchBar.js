3232265bb2a5cc35a17fa03816ae0e26be9b307d
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-11 22:18:21 -0500

    styles, handleclick skeleton for search

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,28 @@
+import { useState } from "react";
+import { IoIosSearch } from "react-icons/io";
+
 function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const handleClick = () => {
+    console.log("i was clicked");
+    // doSearch(searchQuery);
+  };
+
   return (
-    <input className="w-full h-12 mb-4 border-2 p-43" placeholder="Search..." />
+    <div className="flex h-8">
+      <input
+        className="mb-4 border-2 p-4 flex-grow"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button
+        className="w-1/12 flex items-center justify-center border-2 border-l-0"
+        onClick={handleClick}
+      >
+        <IoIosSearch size={25} />
+      </button>
+    </div>
   );
 }
 
