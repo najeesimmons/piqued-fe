7e04f53f9e0578d3049e469161647848313586cb
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-11 22:18:34 -0500

    error log changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,17 +16,14 @@ export async function getStaticProps() {
   const client = createClient(process.env.NEXT_PUBLIC_PEXELS_API_KEY);
   try {
     const response = await client.photos.curated({ per_page: 80 });
-    // console.log("success:", response.photos);
     return {
       props: { photos: response.photos },
-      revalidate: 3600, // Regenerates the page at most once per hour
+      revalidate: 3600,
     };
   } catch (error) {
-    console.log(
-      "problem getting photos in getStaticProps, nobody would even see this"
-    );
+    console.log("ERROR: getStaticProps... nobody can see this 👀");
     return {
-      props: { photos: [] }, // Fallback empty array
+      props: { photos: [] },
       revalidate: 3600,
     };
   }
