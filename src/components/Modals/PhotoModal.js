893c67c42ae84417b8bdb9ef4fec309e70ee9c0e
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-16 13:40:15 -0500

    add comments to modal

--- src/components/Modals/PhotoModal.js
+++ src/components/Modals/PhotoModal.js
@@ -6,6 +6,7 @@ import Section from "../Section/Section";
 import { useEffect } from "react";
 import { usePhoto } from "@/context/PhotoContext";
 import { useRouter } from "next/router";
+import Comments from "../Comments/Comments";
 
 function PhotoModal({ photo }) {
   const { setActivePhoto } = usePhoto();
@@ -21,33 +22,38 @@ function PhotoModal({ photo }) {
     getPhoto();
   }, [id, photo, setActivePhoto]);
 
+  if (!photo) return <p>Loading...</p>;
+
   return (
     <Section>
       <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto w-full flex items-center justify-center">
-        <div className="p-8 border w-[90vw] h-[90vh] shadow-lg bg-white">
-          <div className="text-center">
-            <h3 className="text-2xl font-bold text-gray-900">Modal Title</h3>
-            <div className="mt-2 px-7 py-3">
-              <p className="text-lg text-gray-500">Modal Body</p>
-              {photo && photo.src ? (
-                <Image
-                  src={photo.src.original}
-                  alt="test"
-                  height={200}
-                  width={200}
-                />
-              ) : (
-                <p>Loading photo...</p>
-              )}
-            </div>
-            <div className="flex justify-center mt-4">
-              <Link
-                href="/"
-                className="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
-              >
-                Close
-              </Link>
-            </div>
+        <div className="flex p-4 w-[90vw] h-[90vh] shadow-lg bg-white">
+          <div
+            className="flex items-center justify-center w-full md:w-1/2 h-full"
+            style={{
+              backgroundColor: photo.avg_color,
+            }}
+          >
+            <Image
+              src={photo.src.original}
+              alt={photo.alt}
+              width={photo.width}
+              height={photo.height}
+              style={{
+                maxWidth: "100%",
+                maxHeight: "100%",
+                objectFit: "contain",
+              }}
+            />
+          </div>
+          <div className="flex items-center justify-center w-full md:w-1/2 h-full">
+            <Comments />
+            {/* <Link
+              href="/"
+              className="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
+            >
+              Close
+            </Link> */}
           </div>
         </div>
       </div>
