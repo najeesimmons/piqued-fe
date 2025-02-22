c38886d5bf110222b8dd09afd7a30719c37d6a6e
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-21 21:44:39 -0500

    switch to useRouter from Link to avoid soft rerender of home component

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -1,21 +1,29 @@
+"use client";
+// make client, use router
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/router";
+// TODO: shallow routing from Link which caused soft navigation to home component
+// import Link from "next/link";
 
 function Photo({ photo, priority, setPhoto }) {
+  const router = useRouter();
   return (
-    <Link href={`/?show=true&id=${photo.id}`}>
-      <Image
-        src={photo.src.original}
-        alt={photo.alt}
-        width={0}
-        height={0}
-        className="w-full h-auto cursor-pointer"
-        // TODO: sizes prop must change if breakpoints change in PhotoMasonry
-        sizes="(max-width: 350px) 100vw, (max-width: 750px) 50vw, 33.33vw"
-        {...(priority && { priority })}
-        onClick={() => setPhoto(photo)}
-      />
-    </Link>
+    // <Link href={`/?show=true&id=${photo.id}`}>
+    <Image
+      src={photo.src.original}
+      alt={photo.alt}
+      width={0}
+      height={0}
+      className="w-full h-auto cursor-pointer"
+      // TODO: sizes prop must change if breakpoints change in PhotoMasonry
+      sizes="(max-width: 350px) 100vw, (max-width: 750px) 50vw, 33.33vw"
+      {...(priority && { priority })}
+      onClick={() => {
+        setPhoto(photo);
+        router.push(`/?show=true&id=${photo.id}`, undefined, { shallow: true });
+      }}
+    />
+    // </Link>
   );
 }
 
