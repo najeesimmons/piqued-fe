e07bcc6a8c2b27a30a36706ca7cc13f6ddad1c9b
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-27 19:51:19 -0500

    debugged document not found error with useffect

--- src/components/Modals/ErrorModal/ErrorModal.js
+++ src/components/Modals/ErrorModal/ErrorModal.js
@@ -1,10 +1,19 @@
 "use client";
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 function ErrorModal({ setisError }) {
+  const [modalRoot, setModalRoot] = useState(null);
+
+  useEffect(() => {
+    setModalRoot(document.getElementById("modal-root"));
+  }, []);
+
   const handleClose = () => {
     setisError(false);
   };
+
+  if (!modalRoot) return null;
 
   return ReactDOM.createPortal(
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto w-full flex items-center justify-center z-[9999]">
@@ -13,7 +22,7 @@ function ErrorModal({ setisError }) {
         <button onClick={handleClose}>Exit</button>
       </div>
     </div>,
-    document.getElementById("modal-root")
+    modalRoot
   );
 }
 
