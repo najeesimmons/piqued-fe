b1abbae0d986a331b3d9aa27c7f1465af458d90d
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-27 20:44:37 -0500

    add margin to ErrorModal h1

--- src/components/Modals/ErrorModal/ErrorModal.js
+++ src/components/Modals/ErrorModal/ErrorModal.js
@@ -44,7 +44,7 @@ function ErrorModal({ isError, setisError }) {
   return ReactDOM.createPortal(
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto w-full flex items-center justify-center z-[9999]">
       <div className="flex flex-col p-4 w-[50vw] h-auto shadow-lg bg-white">
-        <h1 className="text-xl font-bold">Oops! 🙈</h1>
+        <h1 className="mx-auto text-xl font-bold">Oops! 🙈</h1>
         <h2 className="mt-2">
           We ran into a little trouble loading more photos. Please close this
           window and scroll down to try again.
