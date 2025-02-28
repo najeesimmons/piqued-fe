5a6f9bc1ddd5d523227f5c753d1e3c285b56536b
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-27 19:38:25 -0500

    add handleclose

--- src/components/Modals/ErrorModal/ErrorModal.js
+++ src/components/Modals/ErrorModal/ErrorModal.js
@@ -1,13 +1,15 @@
 import ReactDOM from "react-dom";
 
-function ErrorModal() {
+function ErrorModal({ setisError }) {
+  const handleClose = () => {
+    setisError(false);
+  };
+
   return ReactDOM.createPortal(
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto w-full flex items-center justify-center z-[9999]">
-      <div className="flex flex-col md:flex-row p-4 w-[90vw] h-auto md:h-[90vh] shadow-lg bg-white">
+      <div className="flex flex-col p-4 w-[50vw] h-auto shadow-lg bg-white">
         <h1>Error</h1>
-        <button onClick={() => console.log("close the error modal")}>
-          Exit
-        </button>
+        <button onClick={handleClose}>Exit</button>
       </div>
     </div>,
     document.getElementById("modal-root")
