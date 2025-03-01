a1af5c1bd17cd8590cc9ccd17d46d5785e630e70
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-28 23:20:51 -0500

    nav css

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,16 @@
 "use client";
+// import { FaExclamation } from "react-icons/fa";
 import Link from "next/link";
 
 function Navigation() {
   return (
-    <nav className="flex flex-row items-center justify-between font-semibold">
-      <span>homeLogo</span>
+    <nav className="py-2 flex flex-row items-center justify-between font-semibold text-sm">
+      <span className="fond-semibold">piqued</span>
       <ul className="flex flex-row gap-2">
-        <li>
-          <Link href={"/login"}>login</Link>
+        <li className="after:content-['|'] after:mx-2 last:after:content-none font-semibold">
+          <Link href={"/login"}>log in</Link>
         </li>
-        <li>
+        <li className="font-semibold">
           <Link href={"/login"}>sign up</Link>
         </li>
       </ul>
