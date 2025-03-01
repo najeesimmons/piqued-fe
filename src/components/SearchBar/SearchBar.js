0392324f3dacf067207e0a8d0386dca738067b13
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-28 23:20:59 -0500

    searchbar css

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -13,7 +13,7 @@ function SearchBar({ getSearchPhotos, searchTerm, setSearchTerm }) {
   }, [getSearchPhotos]);
 
   return (
-    <div className="flex h-8">
+    <div className="flex h-8 mb-4">
       <input
         className="mb-4 border-2 p-4 flex-grow"
         placeholder="Search..."
@@ -21,7 +21,7 @@ function SearchBar({ getSearchPhotos, searchTerm, setSearchTerm }) {
         onChange={(e) => setSearchTerm(e.target.value)}
       />
       <button
-        className="w-1/12 flex items-center justify-center border-2 border-l-0"
+        className="w-1/12 flex py-4 items-center justify-center border-2 border-l-0"
         onClick={getSearchPhotos}
       >
         <IoIosSearch size={25} />
