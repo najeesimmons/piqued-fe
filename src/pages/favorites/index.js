240f5c37a218a1ee0f9cbf956571ccdfee3d5149
najeesimmons <najee.bryant.simmons@gmail.com>
2025-07-04 13:39:36 -0400

    favorites

---
+++ src/pages/favorites/index.js
@@ -0,0 +1,64 @@
+"use client";
+import dynamic from "next/dynamic";
+import { getFavorites } from "../../../lib/favorite";
+import Navigation from "@/components/Navigation/Navigation";
+import { useEffect, useState } from "react";
+
+const DynamicPhotoMasonry = dynamic(
+  () => import("@/components/Masonry/PhotoMasonry"),
+  {
+    ssr: false,
+    // can't use ssr b/c component relies on uses browser-only APIs (e.g., window, document etc.)
+  }
+);
+
+function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+  const [photo, setPhoto] = useState();
+  const [hasMore, sethasMore] = useState();
+  const [startRange, setstartRange] = useState(0);
+  const [limit] = useState(9);
+  const [endRange, setEndRange] = useState(limit);
+
+  const getFirstFavorites = async () => {
+    const response = await getFavorites(startRange, limit);
+    setFavorites(response);
+    //importnant edgecase needs to be caught here, what if we end on a set of 10 but there happen t0 be no more photos after?
+    if (favorites.length < limit) {
+      sethasMore(true);
+    } else {
+      sethasMore(false);
+    }
+  };
+
+  const getNextFavorites = async () => {
+    const response = await getFavorites(startRange, endRange);
+    console.log("secnd reponse", response);
+    setFavorites((prev) => [...prev, ...response]);
+    setstartRange((prev) => prev + favorites.length);
+    setEndRange((prev) => prev + limit);
+    if (favorites.length < limit) {
+      sethasMore(true);
+    } else {
+      sethasMore(false);
+    }
+  };
+
+  useEffect(() => {
+    getFirstFavorites();
+  }, []);
+
+  return (
+    <>
+      <Navigation />
+      <DynamicPhotoMasonry
+        getNextPhotos={getNextFavorites}
+        hasMore={hasMore}
+        photos={favorites}
+        setPhoto={setPhoto}
+      />
+    </>
+  );
+}
+
+export default Favorites;
