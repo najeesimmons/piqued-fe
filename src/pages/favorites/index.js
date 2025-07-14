ef6e7f279b0d587d3d36a3c5f737c2ef9715853f
najeesimmons <najee.bryant.simmons@gmail.com>
2025-07-14 11:26:05 -0400

    simplify state and modify functions to utilize count

--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -8,40 +8,34 @@ const DynamicPhotoMasonry = dynamic(
   () => import("@/components/Masonry/PhotoMasonry"),
   {
     ssr: false,
-    // can't use ssr b/c component relies on uses browser-only APIs (e.g., window, document etc.)
+    //component relies on uses browser-only APIs
   }
 );
 
 function Favorites() {
+  const [, setPhoto] = useState();
+  const [hasMore, setHasMore] = useState(false);
   const [favorites, setFavorites] = useState([]);
-  const [photo, setPhoto] = useState();
-  const [hasMore, sethasMore] = useState();
-  const [startRange, setstartRange] = useState(0);
-  const [limit] = useState(9);
-  const [endRange, setEndRange] = useState(limit);
+  const [start, setStart] = useState(0);
+
+  const LIMIT = 7;
+  const end = start + LIMIT - 1;
 
   const getFirstFavorites = async () => {
-    const response = await getFavorites(startRange, limit);
-    setFavorites(response);
-    //importnant edgecase needs to be caught here, what if we end on a set of 10 but there happen t0 be no more photos after?
-    if (favorites.length < limit) {
-      sethasMore(true);
-    } else {
-      sethasMore(false);
-    }
+    const { favorites, count } = await getFavorites(start, end); //0,6
+    setFavorites(favorites);
+    setHasMore(end + 1 < count);
+    setStart(end + 1);
   };
 
   const getNextFavorites = async () => {
-    const response = await getFavorites(startRange, endRange);
-    console.log("second reponse", response);
-    setFavorites((prev) => [...prev, ...response]);
-    setstartRange((prev) => prev + favorites.length);
-    setEndRange((prev) => prev + limit);
-    if (response.length < limit) {
-      sethasMore(false);
-    } else {
-      sethasMore(true);
-    }
+    const end = start + LIMIT - 1;
+
+    const { favorites: nextFavorites, count } = await getFavorites(start, end);
+
+    setFavorites((prev) => [...prev, ...nextFavorites]);
+    setStart(end + 1);
+    setHasMore(end + 1 < count);
   };
 
   useEffect(() => {
