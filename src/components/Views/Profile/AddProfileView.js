aeca67c5c421f62761a0f408cff0fa2209083567
najeesimmons <najee.bryant.simmons@gmail.com>
2025-07-23 20:45:04 -0400

    add profile view

---
+++ src/components/Views/Profile/AddProfileView.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { insertProfile } from "../../../../lib/profile/profile";
+
+export default function AddProfileView({ user }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+
+  const handleSubmit = async () => {
+    const { data, error } = await insertProfile(
+      firstName,
+      lastName,
+      username,
+      user.id
+    );
+
+    if (error) {
+      console.log("insert profile didn't work");
+    } else {
+      console.log("profile worked");
+    }
+  };
+
+  return (
+    <form className="mt-2 mx-auto space-y-2 w-3/4" onSubmit={handleSubmit}>
+      <label className="block">first name</label>
+      <input className="w-full h-8 border" placeholder="first name" />
+      {/* <br /> */}
+      <label className="block">last name</label>
+      <input className="w-full h-8 border" placeholder="last name" />
+      {/* <br /> */}
+      <label className="block">username</label>
+      <input className="w-full h-8 border" placeholder="username" required />
+      {/* <br /> */}
+      <button
+        className="bg-black font-semibold !mt-6 mx-auto p-2 text-white w-full"
+        type="submit"
+      >
+        submit
+      </button>
+    </form>
+  );
+}
