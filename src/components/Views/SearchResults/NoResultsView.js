f4fe144eb33f37517363bd75e5b3774f418a26ca
najeesimmons <najee.bryant.simmons@gmail.com>
2025-07-26 18:27:47 -0400

    make component more flexible for favorites

--- src/components/Views/SearchResults/NoResultsView.js
+++ src/components/Views/SearchResults/NoResultsView.js
@@ -1,12 +1,19 @@
-import React from "react";
-
-function NoResultsView() {
+function NoResultsView({ type = "search" }) {
   return (
     <div className="flex flex-col p-4 h-auto bg-white">
       <h1 className="mx-auto text-xl font-bold">Oops! 🙈</h1>
-      <h2 className="mt-2 text-center">
-        There were no results for this search. Try something new!
-      </h2>
+      {type === "search" && (
+        <h2 className="mt-2 text-center">
+          There were no results for this search. Try something new!
+        </h2>
+      )}
+
+      {type === "favorites" && (
+        <h2 className="mt-2 text-center">
+          You have not favorited any photos yet. Go find some photos
+          that&apos;ll Pique your intererst!
+        </h2>
+      )}
     </div>
   );
 }
