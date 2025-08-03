2bebe43d936b22a98edb8ad6c9cd757cf8416c90
najeesimmons <najee.bryant.simmons@gmail.com>
2025-08-02 20:42:02 -0400

    move close button from LoginSignUpView to Login & Signup components

--- src/components/Modals/LoginOrSignupModal/LoginOrSignupView.js
+++ src/components/Modals/LoginOrSignupModal/LoginOrSignupView.js
@@ -2,7 +2,6 @@ import Login from "@/components/Views/Auth/Login";
 import ReactDOM from "react-dom";
 import Section from "@/components/Section/Section";
 import Signup from "@/components/Views/Auth/Signup";
-import { IoCloseSharp } from "react-icons/io5";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
@@ -34,31 +33,15 @@ export default function LoginOrSignupModal({
   return ReactDOM.createPortal(
     <Section>
       <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto w-full flex items-center justify-center z-[9999]">
-        <div className="flex flex-col md:flex-row p-4 w-[90vw] h-auto md:h-[90vh] shadow-lg relative">
-          <button
-            onClick={() => {
-              setIsShowAuthCta(false);
-              setDisableComment?.(false);
-              const { redirect, ...rest } = router.query;
-              const newQuery = new URLSearchParams(rest).toString();
-              const newUrl = `${router.pathname}${
-                newQuery ? `?${newQuery}` : ""
-              }`;
-
-              router.replace(newUrl, undefined, { shallow: true });
-            }}
-            className="absolute top-4 left-4 text-3xl z-[10000]"
-            aria-label="Close Modal"
-          >
-            <IoCloseSharp color="black" size={35} />
-          </button>
-
+        <div className="p-4 w-[90vw] h-auto md:h-[90vh] shadow-lg">
           {authMode === "login" ? (
-            <Login
-              setAuthMode={setAuthMode}
-              setDisableComment={setDisableComment}
-              setIsShowAuthCta={setIsShowAuthCta}
-            />
+            <>
+              <Login
+                setAuthMode={setAuthMode}
+                setDisableComment={setDisableComment}
+                setIsShowAuthCta={setIsShowAuthCta}
+              />
+            </>
           ) : (
             <Signup
               setAuthMode={setAuthMode}
