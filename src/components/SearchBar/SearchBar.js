c798575811f2748fd1149356ebcb1fba696570b0
najeesimmons <najee.bryant.simmons@gmail.com>
2025-08-06 19:34:20 -0400

    routing + calling getFirstPhotos

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,16 @@
+"use client";
+import { IoChevronBackSharp } from "react-icons/io5";
 import { IoIosSearch } from "react-icons/io";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-function SearchBar({ getSearchPhotos, isDisabled, searchTerm, setSearchTerm }) {
+function SearchBar({
+  getFirstPhotos,
+  getSearchPhotos,
+  isDisabled,
+  searchTerm,
+  setSearchTerm,
+}) {
   useEffect(() => {
     const handleKeyDown = async (e) => {
       if (isDisabled) return;
@@ -13,8 +22,23 @@ function SearchBar({ getSearchPhotos, isDisabled, searchTerm, setSearchTerm }) {
     return () => window.removeEventListener("keydown", handleKeyDown);
   }, [getSearchPhotos, isDisabled, searchTerm]);
 
+  const router = useRouter();
+
   return (
     <div className="flex h-8 mb-4">
+      {searchTerm && (
+        <button
+          className="w-1/12 flex py-4 items-center justify-center border-2 border-r-0"
+          onClick={async () => {
+            router.push("/");
+            console.log("now how do I get curated photos back?");
+            await getFirstPhotos();
+          }}
+        >
+          <IoChevronBackSharp size={25} />
+        </button>
+      )}
+
       <input
         className="mb-4 border-2 p-4 flex-grow bg-inherit"
         placeholder="Search..."
