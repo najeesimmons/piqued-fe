b911674343d56b6350d74ce8b17021650fc7b3a2
najeesimmons <najee.bryant.simmons@gmail.com>
2025-08-09 11:14:01 -0400

    remove mt from in photomodal Img

--- src/components/Views/PhotoView.js
+++ src/components/Views/PhotoView.js
@@ -44,7 +44,6 @@ function PhotoView({ displayPhoto, handleClose, handleFavorite }) {
           objectFit: "contain",
           opacity: isReadyToRender ? 1 : 0,
           transition: "opacity 0.4s ease-in-out",
-          marginTop: 5,
         }}
         onLoad={() => setIsReadyToRender(true)}
       />
