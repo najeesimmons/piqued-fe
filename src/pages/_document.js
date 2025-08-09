9dfd1ffe79924d3ec50e505e6c79d1461ee10e19
najeesimmons <najee.bryant.simmons@gmail.com>
2025-08-09 09:22:38 -0400

    custom favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.svg?v=2" type="image/svg+xml" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png?v=2"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png?v=2"
+        />
+        {/* <link rel="alternate icon" href="/favicon.ico" /> */}
+      </Head>
       <body className="antialiased">
         <div id="modal-root"></div>
         <Main />
